685ca1bc84657d90d7caa5d7d90427cd678458d5
GuoHao <hubris6965@hotmail.com>
2012-10-22 11:38:45 +0800

    修改代码格式

--- VERSION1.0/SQLite3/inc/codec.h
+++ VERSION1.0/SQLite3/inc/codec.h
@@ -1,22 +1,23 @@
 /*
-///////////////////////////////////////////////////////////////////////////////
-// Name:        codec.h
-// Purpose:     
-// Author:      Ulrich Telle
-// Modified by:
-// Created:     2006-12-06
-// Copyright:   (c) Ulrich Telle
-// Licence:     wxWindows licence
-///////////////////////////////////////////////////////////////////////////////
+ ///////////////////////////////////////////////////////////////////////////////
+ // Name:        codec.h
+ // Purpose:     
+ // Author:      Ulrich Telle
+ // Modified by:
+ // Created:     2006-12-06
+ // Copyright:   (c) Ulrich Telle
+ // Licence:     wxWindows licence
+ ///////////////////////////////////////////////////////////////////////////////
 
-/// \file codec.h Interface of the codec class
-*/
+ /// \file codec.h Interface of the codec class
+ */
 
 #ifndef _CODEC_H_
 #define _CODEC_H_
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #if defined(__BORLANDC__)
@@ -28,13 +29,13 @@ extern "C" {
 #endif
 
 /*
-// ATTENTION: Macro similar to that in pager.c
-// TODO: Check in case of new version of SQLite
-*/
+ // ATTENTION: Macro similar to that in pager.c
+ // TODO: Check in case of new version of SQLite
+ */
 #define WX_PAGER_MJ_PGNO(x) ((PENDING_BYTE/(x))+1)
 
 #ifdef __cplusplus
-}  /* End of the 'extern "C"' block */
+} /* End of the 'extern "C"' block */
 #endif
 
 #include "rijndael.h"
@@ -55,15 +56,15 @@ extern "C" {
 
 typedef struct _Codec
 {
-  int           m_isEncrypted;
-  int           m_hasReadKey;
-  unsigned char m_readKey[KEYLENGTH];
-  int           m_hasWriteKey;
-  unsigned char m_writeKey[KEYLENGTH];
-  Rijndael*     m_aes;
+	int m_isEncrypted;
+	int m_hasReadKey;
+	unsigned char m_readKey[KEYLENGTH];
+	int m_hasWriteKey;
+	unsigned char m_writeKey[KEYLENGTH];
+	Rijndael* m_aes;
 
-  Btree*        m_bt; /* Pointer to B-tree used by DB */
-  unsigned char m_page[SQLITE_MAX_PAGE_SIZE+8];
+	Btree* m_bt; /* Pointer to B-tree used by DB */
+	unsigned char m_page[SQLITE_MAX_PAGE_SIZE + 8];
 } Codec;
 
 void CodecInit(Codec* codec);
@@ -73,9 +74,11 @@ void CodecCopy(Codec* codec, Codec* other);
 
 void CodecGenerateReadKey(Codec* codec, char* userPassword, int passwordLength);
 
-void CodecGenerateWriteKey(Codec* codec, char* userPassword, int passwordLength);
+void CodecGenerateWriteKey(Codec* codec, char* userPassword,
+		int passwordLength);
 
-void CodecEncrypt(Codec* codec, int page, unsigned char* data, int len, int useWriteKey);
+void CodecEncrypt(Codec* codec, int page, unsigned char* data, int len,
+		int useWriteKey);
 
 void CodecDecrypt(Codec* codec, int page, unsigned char* data, int len);
 
@@ -92,24 +95,26 @@ int CodecHasWriteKey(Codec* codec);
 Btree* CodecGetBtree(Codec* codec);
 unsigned char* CodecGetPageBuffer(Codec* codec);
 
-void CodecGenerateEncryptionKey(Codec* codec, char* userPassword, int passwordLength, 
-                                unsigned char encryptionKey[KEYLENGTH]);
+void CodecGenerateEncryptionKey(Codec* codec, char* userPassword,
+		int passwordLength, unsigned char encryptionKey[KEYLENGTH]);
 
-void CodecPadPassword(Codec* codec, char* password, int pswdlen, unsigned char pswd[32]);
+void CodecPadPassword(Codec* codec, char* password, int pswdlen,
+		unsigned char pswd[32]);
 
 void CodecRC4(Codec* codec, unsigned char* key, int keylen,
-              unsigned char* textin, int textlen,
-         unsigned char* textout);
+		unsigned char* textin, int textlen, unsigned char* textout);
 
-void CodecGetMD5Binary(Codec* codec, unsigned char* data, int length, unsigned char* digest);
+void CodecGetMD5Binary(Codec* codec, unsigned char* data, int length,
+		unsigned char* digest);
 
 #if CODEC_TYPE == CODEC_TYPE_AES256
 void CodecGetSHABinary(Codec* codec, unsigned char* data, int length, unsigned char* digest);
 #endif
-  
+
 void CodecGenerateInitialVector(Codec* codec, int seed, unsigned char iv[16]);
 
-void CodecAES(Codec* codec, int page, int encrypt, unsigned char encryptionKey[KEYLENGTH],
-              unsigned char* datain, int datalen, unsigned char* dataout);
+void CodecAES(Codec* codec, int page, int encrypt,
+		unsigned char encryptionKey[KEYLENGTH], unsigned char* datain,
+		int datalen, unsigned char* dataout);
 
 #endif
--- VERSION1.0/SQLite3/src/sqlite3secure.c
+++ VERSION1.0/SQLite3/src/sqlite3secure.c
@@ -18,31 +18,33 @@
 #ifdef SQLITE_HAS_CODEC
 
 /*
-** Get the codec argument for this pager
-*/
+ ** Get the codec argument for this pager
+ */
 
 void* mySqlite3PagerGetCodec(
-  Pager *pPager
-){
+		Pager *pPager
+)
+{
 #if (SQLITE_VERSION_NUMBER >= 3006016)
-  return sqlite3PagerGetCodec(pPager);
+	return sqlite3PagerGetCodec(pPager);
 #else
-  return (pPager->xCodec) ? pPager->pCodecArg : NULL;
+	return (pPager->xCodec) ? pPager->pCodecArg : NULL;
 #endif
 }
 
 /*
-** Set the codec argument for this pager
-*/
+ ** Set the codec argument for this pager
+ */
 
 void mySqlite3PagerSetCodec(
-  Pager *pPager,
-  void *(*xCodec)(void*,void*,Pgno,int),
-  void (*xCodecSizeChng)(void*,int,int),
-  void (*xCodecFree)(void*),
-  void *pCodec
-){
-  sqlite3PagerSetCodec(pPager, xCodec, xCodecSizeChng, xCodecFree, pCodec);
+		Pager *pPager,
+		void *(*xCodec)(void*,void*,Pgno,int),
+		void (*xCodecSizeChng)(void*,int,int),
+		void (*xCodecFree)(void*),
+		void *pCodec
+)
+{
+	sqlite3PagerSetCodec(pPager, xCodec, xCodecSizeChng, xCodecFree, pCodec);
 }
 
 #include "rijndael.c"
@@ -58,44 +60,44 @@ void mySqlite3PagerSetCodec(
 #include "extensionfunctions.c"
 
 SQLITE_API int sqlite3_open(
-  const char *filename,   /* Database filename (UTF-8) */
-  sqlite3 **ppDb          /* OUT: SQLite db handle */
+		const char *filename, /* Database filename (UTF-8) */
+		sqlite3 **ppDb /* OUT: SQLite db handle */
 )
 {
-  int ret = sqlite3_open_internal(filename, ppDb);
-  if (ret == 0)
-  {
-    RegisterExtensionFunctions(*ppDb);
-  }
-  return ret;
+	int ret = sqlite3_open_internal(filename, ppDb);
+	if (ret == 0)
+	{
+		RegisterExtensionFunctions(*ppDb);
+	}
+	return ret;
 }
 
 SQLITE_API int sqlite3_open16(
-  const void *filename,   /* Database filename (UTF-16) */
-  sqlite3 **ppDb          /* OUT: SQLite db handle */
+		const void *filename, /* Database filename (UTF-16) */
+		sqlite3 **ppDb /* OUT: SQLite db handle */
 )
 {
-  int ret = sqlite3_open16_internal(filename, ppDb);
-  if (ret == 0)
-  {
-    RegisterExtensionFunctions(*ppDb);
-  }
-  return ret;
+	int ret = sqlite3_open16_internal(filename, ppDb);
+	if (ret == 0)
+	{
+		RegisterExtensionFunctions(*ppDb);
+	}
+	return ret;
 }
 
 SQLITE_API int sqlite3_open_v2(
-  const char *filename,   /* Database filename (UTF-8) */
-  sqlite3 **ppDb,         /* OUT: SQLite db handle */
-  int flags,              /* Flags */
-  const char *zVfs        /* Name of VFS module to use */
+		const char *filename, /* Database filename (UTF-8) */
+		sqlite3 **ppDb, /* OUT: SQLite db handle */
+		int flags, /* Flags */
+		const char *zVfs /* Name of VFS module to use */
 )
 {
-  int ret = sqlite3_open_v2_internal(filename, ppDb, flags, zVfs);
-  if (ret == 0)
-  {
-    RegisterExtensionFunctions(*ppDb);
-  }
-  return ret;
+	int ret = sqlite3_open_v2_internal(filename, ppDb, flags, zVfs);
+	if (ret == 0)
+	{
+		RegisterExtensionFunctions(*ppDb);
+	}
+	return ret;
 }
 
 #endif
