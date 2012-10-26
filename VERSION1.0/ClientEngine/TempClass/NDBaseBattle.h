02f77b446e33d99015f9e4bbefdf6bf8fef28548
GuoHao <hubris6965@hotmail.com>
2012-10-26 14:33:16 +0800

    删除不需要的NDBaseBattle.cpp，重新修整了新类

--- VERSION1.0/ClientEngine/TempClass/NDBaseBattle.h
+++ VERSION1.0/ClientEngine/TempClass/NDBaseBattle.h
@@ -1,55 +1,59 @@
-// /*
-// *
-// */
-// 
-// #ifndef NDBASEBATTLE_H
-// #define NDBASEBATTLE_H
-// 
-// #include "NDObject.h"
-// #include "define.h"
-// #include "NDUITableLayer.h"
-// #include "NDDataSource.h"
-// 
-// class BattleSkill;
-// class FighterStatus;
-// class FightAction;
-// 
-// class NDBaseBattle:public NDObject
-// {
-// 	DECLARE_CLASS(NDBaseBattle);
-// 
-// public:
-// 
-// 	NDBaseBattle();
-// 	virtual ~NDBaseBattle();
-// 
+/*
+ *
+ */
+
+#ifndef NDBASEBATTLE_H
+#define NDBASEBATTLE_H
+
+#include "NDObject.h"
+#include "define.h"
+#include "NDUITableLayer.h"
+#include "NDDataSource.h"
+
+class BattleSkill;
+class FighterStatus;
+class FightAction;
+
+NS_NDENGINE_BGN
+
+class NDBaseBattle: public NDObject
+{
+	DECLARE_CLASS (NDBaseBattle);
+
+public:
+
+	NDBaseBattle(){}
+	virtual ~NDBaseBattle(){}
+
 // 	virtual Fighter* GetMainUser() = 0;
 // 	virtual Fighter* getMainEudemon() = 0;
-// 	virtual bool IsPracticeBattle() = 0;
-// 	virtual int GetBattleType() = 0;
-// 
-// 	virtual void SetTurn(int turn) = 0;
-// 	virtual void StartFight() = 0;
-// 	virtual void RestartFight() = 0;
-// 	virtual void dealWithCommand() = 0;
-// 	virtual void AddCommand(Command* cmd) = 0;
-// 
-// 	virtual void AddActionCommand(FightAction* action) = 0;
-// 	virtual void InitSpeedBar() = 0;
-// 	virtual void InitEudemonOpt() = 0;
-// 	virtual void Initialization(int action) = 0;
-// 	virtual bool TouchEnd(NDTouch* touch)  = 0;
-// 	virtual void draw() = 0;
-// 	virtual void drawSubAniGroup() = 0;
-// 	virtual void OnTableLayerCellSelected(NDUITableLayer* table,
-// 		NDUINode* cell,unsigned int cellIndex, NDSection* section) = 0;
-// 
-// 	virtual void AddFighter(Fighter* f) = 0;
-// 
-// 	virtual void SetFighterOnline(int idFighter, bool bOnline) = 0;
-// 
-// protected:
-// private:
-// };
-// 
-// #endif
+	virtual bool IsPracticeBattle() = 0;
+	virtual int GetBattleType() = 0;
+
+	virtual void SetTurn(int turn) = 0;
+	virtual void StartFight() = 0;
+	virtual void RestartFight() = 0;
+	virtual void dealWithCommand() = 0;
+	//virtual void AddCommand(Command* cmd) = 0;
+
+	virtual void AddActionCommand(FightAction* action) = 0;
+	virtual void InitSpeedBar() = 0;
+	virtual void InitEudemonOpt() = 0;
+	virtual void Initialization(int action) = 0;
+	virtual bool TouchEnd(NDTouch* touch) = 0;
+	virtual void draw() = 0;
+	virtual void drawSubAniGroup() = 0;
+	virtual void OnTableLayerCellSelected(NDUITableLayer* table, NDUINode* cell,
+			unsigned int cellIndex, NDSection* section) = 0;
+
+	//virtual void AddFighter(Fighter* f) = 0;
+
+	virtual void SetFighterOnline(int idFighter, bool bOnline) = 0;
+
+protected:
+private:
+};
+
+NS_NDENGINE_END
+
+#endif
