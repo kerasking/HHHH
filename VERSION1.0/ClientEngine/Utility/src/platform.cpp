c5ee5e22d9b578ede001d67e0e6b6a1073b3fa70
ZhangDi <z176230704@gmail.com>
2012-11-07 17:41:09 +0800

    修正NPC对话框中内容无法显示的问题

--- VERSION1.0/ClientEngine/Utility/src/platform.cpp
+++ VERSION1.0/ClientEngine/Utility/src/platform.cpp
@@ -54,6 +54,8 @@ CGSize getStringSize(const char* pszStr, uint fontSize)
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
 		NSString str = [NSString stringWithUTF8String:pszStr];
 		sz = [str sizeWithFont:[UIFont fontWithName:FONT_NAME size:fontSize]];
+#else
+		sz = CGSizeMake(24.0f, 29.0f);
 #endif
 	}
 
