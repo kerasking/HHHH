0f3ba2d15bf80df934631fc1a22b86b51b71e94d
liujh <liujh007@gmail.com>
2013-01-22 23:31:55 +0800

    解决Home键返回社区页面丢失的问题

--- version1.0/ClientLancher/proj.android/src/tw/mobage/g23000052/MainActivity.java
+++ version1.0/ClientLancher/proj.android/src/tw/mobage/g23000052/MainActivity.java
@@ -2,5 +2,21 @@ package tw.mobage.g23000052;
 
 import org.DeNA.DHLJ.DaHuaLongJiang;
 
-public class MainActivity extends DaHuaLongJiang {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {	
+	protected void onCreate(Bundle savedInstanceState) {
+	Log.d("init", "@@ MainActivity.onCreate()");
+    super.onCreate(savedInstanceState);
+    
+    Intent i = new Intent(this, DaHuaLongJiang.class);
+	i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+    i.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+    i.addFlags(Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);
+    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+	startActivity(i);
+	}
 }
