fefd193ea5ac0d61b792d9f00211f5241091367d
liujh <liujh007@gmail.com>
2013-01-23 00:46:59 +0800

    退出机制修改

--- version1.0/ClientLancher/proj.android/src/tw/mobage/g23000052/MainActivity.java
+++ version1.0/ClientLancher/proj.android/src/tw/mobage/g23000052/MainActivity.java
@@ -3,20 +3,32 @@ package tw.mobage.g23000052;
 import org.DeNA.DHLJ.DaHuaLongJiang;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends Activity {	
+	private static MainActivity mActivity;
+	
 	protected void onCreate(Bundle savedInstanceState) {
-	Log.d("init", "@@ MainActivity.onCreate()");
-    super.onCreate(savedInstanceState);
+		Log.d("init", "@@ MainActivity.onCreate()");
+		super.onCreate(savedInstanceState);
+		mActivity = this;
     
-    Intent i = new Intent(this, DaHuaLongJiang.class);
-	i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-    i.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-    i.addFlags(Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);
-    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-	startActivity(i);
+		Intent i = new Intent(this, DaHuaLongJiang.class);
+		i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+		i.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+		i.addFlags(Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);
+		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(i);
+	}
+	
+	public static void onExit()
+	{
+//		android.os.Process.killProcess(pid);
+		mActivity.finish();
+		System.exit(0);
 	}
 }
