41cb2876d5bb30eb50bb5fe097c60ac606d8fc1a
zhangwq <zhang_wei_qiang@sina.com>
2013-02-20 15:14:25 +0800

    字符字宽细调（仅用于android2.x）

--- VERSION1.0/ClientLancher/proj.android/src/org/cocos2dx/lib/NDSpecialCharWidth.java
+++ VERSION1.0/ClientLancher/proj.android/src/org/cocos2dx/lib/NDSpecialCharWidth.java
@@ -34,38 +34,38 @@ public class NDSpecialCharWidth
 	
 	private static void doInit()
 	{
-		add('A', 1.00f); 	add('a', 0.55f);
-		add('B', 1.00f); 	add('b', 0.60f);
-		add('C', 1.00f); 	add('c', 0.55f);
-		add('D', 1.00f); 	add('d', 0.55f);
-		add('E', 1.00f); 	add('e', 0.60f);
-		add('F', 1.00f); 	add('f', 0.60f);
-		add('G', 1.00f); 	add('g', 0.60f);
+		add('A', 0.90f); 	add('a', 0.75f);
+		add('B', 0.90f); 	add('b', 0.70f);
+		add('C', 0.90f); 	add('c', 0.70f);
+		add('D', 0.90f); 	add('d', 0.70f);
+		add('E', 0.90f); 	add('e', 0.70f);
+		add('F', 0.90f); 	add('f', 0.65f);
+		add('G', 0.90f); 	add('g', 0.70f);
 		
-		add('H', 1.00f); 	add('h', 0.60f);
-		add('I', 0.80f); 	add('i', 0.50f);
-		add('J', 0.70f); 	add('j', 0.40f);
-		add('K', 1.00f); 	add('k', 0.60f);
-		add('L', 1.00f); 	add('l', 0.40f);
-		add('M', 1.00f); 	add('m', 0.60f);
-		add('N', 1.00f); 	add('n', 0.60f);
+		add('H', 0.90f); 	add('h', 0.70f);
+		add('I', 0.70f); 	add('i', 0.50f);
+		add('J', 0.90f); 	add('j', 0.60f);
+		add('K', 0.90f); 	add('k', 0.70f);
+		add('L', 0.90f); 	add('l', 0.70f);
+		add('M', 1.00f); 	add('m', 0.90f);
+		add('N', 0.90f); 	add('n', 0.70f);
 		
-		add('O', 1.00f); 	add('o', 0.60f);
-		add('P', 1.00f); 	add('p', 0.60f);
-		add('Q', 1.00f); 	add('q', 0.60f);
-		add('R', 1.00f); 	add('r', 0.55f);
-		add('S', 1.00f); 	add('s', 0.60f);
-		add('T', 1.00f); 	add('t', 0.60f);
+		add('O', 0.90f); 	add('o', 0.80f);
+		add('P', 0.90f); 	add('p', 0.7f);
+		add('Q', 0.90f); 	add('q', 0.80f);
+		add('R', 0.90f); 	add('r', 0.80f);
+		add('S', 0.90f); 	add('s', 0.70f);
+		add('T', 0.90f); 	add('t', 0.70f);
 		
-		add('U', 1.00f); 	add('u', 0.60f);
-		add('V', 1.00f); 	add('v', 0.60f);
+		add('U', 0.90f); 	add('u', 0.70f);
+		add('V', 0.90f); 	add('v', 0.70f);
 		add('W', 1.00f); 	add('w', 0.85f);
-		add('X', 1.00f); 	add('x', 0.60f);
-		add('Y', 1.00f); 	add('y', 0.60f);
-		add('Z', 1.00f); 	add('z', 0.60f);
+		add('X', 0.90f); 	add('x', 0.70f);
+		add('Y', 0.90f); 	add('y', 0.70f);
+		add('Z', 0.90f); 	add('z', 0.70f);
 		
-		add('0', 0.85f);
-		add('1', 0.6f);
+		add('0', 0.65f);
+		add('1', 0.5f);
 		add('2', 0.65f);
 		add('3', 0.65f);
 		add('4', 0.65f);
